25b83e042b0de862c73c4ccbb2ac9494dcbc8dda
Amir Firouz <amirhf@gmail.com>
2024-12-08 21:00:27 -0800

    Update db

--- UserManagerAPI/UserManagerAPI/settings.py
+++ UserManagerAPI/UserManagerAPI/settings.py
@@ -122,8 +122,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-SECURE_CROSS_ORIGIN_OPENER_POLICY = None
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
